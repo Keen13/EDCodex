b72f2a9ea69d8fc79bbae8fecdb5ba4428e3bf88
glabular <glu.max06@gmail.com>
2025-06-04 23:47:36 +0200

    UI elements added

--- EDCodex.Panel/PanelUserControl.Designer.cs
+++ EDCodex.Panel/PanelUserControl.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.textBox_logMsgs = new System.Windows.Forms.RichTextBox();
+            this.comboBox_currentRegion = new System.Windows.Forms.ComboBox();
+            this.label_currentRegion = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_logMsgs
@@ -39,19 +41,41 @@
             this.textBox_logMsgs.TabIndex = 0;
             this.textBox_logMsgs.Text = "";
             // 
+            // comboBox_currentRegion
+            // 
+            this.comboBox_currentRegion.FormattingEnabled = true;
+            this.comboBox_currentRegion.Location = new System.Drawing.Point(3, 339);
+            this.comboBox_currentRegion.Name = "comboBox_currentRegion";
+            this.comboBox_currentRegion.Size = new System.Drawing.Size(272, 28);
+            this.comboBox_currentRegion.TabIndex = 1;
+            // 
+            // label_currentRegion
+            // 
+            this.label_currentRegion.AutoSize = true;
+            this.label_currentRegion.Location = new System.Drawing.Point(4, 313);
+            this.label_currentRegion.Name = "label_currentRegion";
+            this.label_currentRegion.Size = new System.Drawing.Size(110, 20);
+            this.label_currentRegion.TabIndex = 2;
+            this.label_currentRegion.Text = "Current region";
+            // 
             // PanelUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label_currentRegion);
+            this.Controls.Add(this.comboBox_currentRegion);
             this.Controls.Add(this.textBox_logMsgs);
             this.Name = "PanelUserControl";
             this.Size = new System.Drawing.Size(886, 600);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.RichTextBox textBox_logMsgs;
+        private System.Windows.Forms.ComboBox comboBox_currentRegion;
+        private System.Windows.Forms.Label label_currentRegion;
     }
 }
